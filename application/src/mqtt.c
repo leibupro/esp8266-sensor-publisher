1fff668ac7178a24e9d1a229f3ac5ac1c764215b
Prosper Leibundgut <leiu@zhaw.ch>
2026-06-23 20:54:07 +0200

    Sending now the real sensor data with the esp8266 wifi adaptor MAC address in the topic name.

--- application/src/mqtt.c
+++ application/src/mqtt.c
@@ -18,14 +18,24 @@
 
 
 #define MAC_ALEN 6U
+#define MAC_STRLEN ( ( 2U * MAC_ALEN ) + ( MAC_ALEN - 1U ) + 1U )
 #define BROKER_URI "mqtt://192.168.1.21:1883"
 #define PUB_TOPIC_BASE "/js8/cellar/hallway/%s/dht"
+#define TOPIC_SPACE 64U
 #define PUB_QOS 1
+#define PUB_DATA_BASE ( \
+    "Humidity: %2hd %%, " \
+    "Temperature: %3hd degree Celsius." \
+)
+#define DATA_SPACE 64U
 
 
 static esp_mqtt_client_handle_t client = NULL;
 static EventGroupHandle_t s_wifi_event_group = NULL;
 static uint8_t wifi_mac[ MAC_ALEN ] = { 0U };
+static char mac_str[ MAC_STRLEN ] = { 0 };
+static char pub_topic[ TOPIC_SPACE ] = { 0 };
+static char pub_data[ DATA_SPACE ] = { 0 };
 static const char* const tag_mqtt_pub = "mqtt_publisher";
 
 
@@ -59,6 +69,23 @@ void mqtt_publisher_init( void )
     esp_read_mac(
         &( wifi_mac[ 0U ] ),
         ESP_MAC_WIFI_STA
+    );
+    ( void )snprintf(
+        &mac_str[ 0U ],
+        MAC_STRLEN,
+        "%02x:%02x:%02x:%02x:%02x:%02x",
+        wifi_mac[ 0U ],
+        wifi_mac[ 1U ],
+        wifi_mac[ 2U ],
+        wifi_mac[ 3U ],
+        wifi_mac[ 4U ],
+        wifi_mac[ 5U ]
+    );
+    ( void )snprintf(
+        &pub_topic[ 0U ],
+        TOPIC_SPACE,
+        PUB_TOPIC_BASE,
+        &mac_str[ 0U ]
     );
     client = esp_mqtt_client_init( &mqtt_cfg );
     esp_mqtt_client_register_event(
@@ -107,19 +134,26 @@ static void publish(
     const data_record_t* const record
 )
 {
-    const char* const topic = "/js8/test";
-    const char* const data = "Foobarbazqux ...";
     if( client )
     {
+        ( void )snprintf(
+            &pub_data[ 0U ],
+            DATA_SPACE,
+            PUB_DATA_BASE,
+            record->humidity,
+            record->temperature
+        );
         /*
          * QoS 1 is the 5th parameter (0, 1, or 2)
          * msg_id > 0 means the message was successfully
-         * queued for delivery.
+         * queued for delivery. The byte lenght parameter
+         * is intentionally set to 0. This means that
+         * length calculates automatically via strlen(data).
          * */
         int msg_id = esp_mqtt_client_publish(
             client,
-            topic,
-            data,
+            pub_topic,
+            pub_data,
             0,
             PUB_QOS,
             0
@@ -133,10 +167,8 @@ static void publish(
             );
             ESP_LOGI(
                 tag_mqtt_pub,
-                "Humidity: %2hd %%, "
-                "Temperature: %3hd degree Celsius.",
-                record->humidity,
-                record->temperature
+                "%s",
+                &pub_data[ 0U ]
             );
         }
     }
